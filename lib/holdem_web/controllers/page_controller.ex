be69273589522de69b73a6ab977109e6795b2653
Rosa Richter <cosmic.lady.rosa@gmail.com>
2025-05-07 18:41:33 -0600

    Create a front page

--- lib/holdem_web/controllers/page_controller.ex
+++ lib/holdem_web/controllers/page_controller.ex
@@ -1,7 +1,22 @@
 defmodule HoldemWeb.PageController do
   use HoldemWeb, :controller
 
+  alias Holdem.Poker.Game
+
   def home(conn, _params) do
-    render(conn, :home)
+    changeset = Game.changeset(%Game{}, %{players: [%{}]})
+
+    currencies =
+      Money.Currency.known_current_currencies()
+      |> Enum.map(fn code ->
+        {:ok, currency} = Money.Currency.currency_for_code(code)
+
+        {
+          Cldr.display_name(currency),
+          Atom.to_string(code)
+        }
+      end)
+
+    render(conn, :home, changeset: changeset, currencies: currencies)
   end
 end
